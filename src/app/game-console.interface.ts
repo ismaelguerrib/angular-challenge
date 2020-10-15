1ba06bf06540c74f2224a857e26e9e45be3d16cc
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2020-10-15 15:56:31 +0200

    style: fill the table with the average sales per weekfor each game console and each year

--- src/app/game-console.interface.ts
+++ src/app/game-console.interface.ts
@@ -2,4 +2,6 @@ export interface GameConsole {
   name: string
   sales2017: number
   sales2018: number
+  averageWeekSales2018: number
+  averageWeekSales2019: number
 }
--- src/app/services/game-console.service.ts
+++ src/app/services/game-console.service.ts
@@ -10,17 +10,23 @@ export class GameConsoleService {
       {
         name: 'Playstation 4',
         sales2017: 20,
-        sales2018: 18
+        sales2018: 18,
+        averageWeekSales2018: 3,
+        averageWeekSales2019: 1.3,
       },
       {
         name: 'X Box One',
         sales2017: 7.62,
-        sales2018: 6.96
+        sales2018: 6.96,
+        averageWeekSales2018: 4.2,
+        averageWeekSales2019: 2.4,
       },
       {
         name: 'Switch',
         sales2017: 0,
-        sales2018: 16.33
+        sales2018: 16.33,
+        averageWeekSales2018: 1,
+        averageWeekSales2019: 2,
       }
     ]
   }
