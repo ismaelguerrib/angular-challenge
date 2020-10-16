d51860e1c27c6ccdf34bc754aac1254467f28866
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2020-10-16 19:13:26 +0200

    feat: find the most and the less sold console and display it in the console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,36 @@ export class AppComponent {
 
 
   constructor(private gameConsoleService: GameConsoleService) {
-    console.log(this.gameConsoles)
+    let mostSoldGameConsole = null;
+    let lessSoldGameConsole = null;
+    var gameConsole1 = [this.gameConsoles[0].name, this.gameConsoles[0].sales2017 + this.gameConsoles[0].sales2018];
+    var gameConsole2 = [this.gameConsoles[1].name, this.gameConsoles[1].sales2017 + this.gameConsoles[1].sales2018];
+    var gameConsole3 = [this.gameConsoles[2].name, this.gameConsoles[2].sales2017 + this.gameConsoles[2].sales2018];
+
+
+    function sortByMostSold() {
+      if (gameConsole1.slice(1) > gameConsole2.slice(1) && gameConsole1.slice(1) > gameConsole3.slice(1))
+        mostSoldGameConsole = gameConsole1[0];
+
+      else if (gameConsole2.slice(1) > gameConsole1.slice(1) && gameConsole2.slice(1) > gameConsole3.slice(1))
+        mostSoldGameConsole = gameConsole2[0];
+
+      else mostSoldGameConsole = gameConsole3[0];
+    }
+    sortByMostSold()
+    console.log(mostSoldGameConsole)
+
+    function sortByLessSold() {
+      if (gameConsole1.slice(1) < gameConsole2.slice(1) && gameConsole1.slice(1) < gameConsole3.slice(1))
+        lessSoldGameConsole = gameConsole1[0];
+
+      else if (gameConsole2.slice(1) < gameConsole1.slice(1) && gameConsole2.slice(1) < gameConsole3.slice(1))
+        lessSoldGameConsole = gameConsole2[0];
+
+      else mostSoldGameConsole = gameConsole3[0];
+    }
+    sortByLessSold()
+    console.log(lessSoldGameConsole)
   }
+
 }
